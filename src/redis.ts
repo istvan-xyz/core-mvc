fe33aa908b68d5a1cf85fa9ccf10c8f4640893f1
István Antal <istvan@antal.xyz>
2022-06-23 08:50:43 +0100

    Use explicit export for type

--- src/redis.ts
+++ src/redis.ts
@@ -1,7 +1,5 @@
 import type { createClient } from '@node-redis/client';
 
-type RedisClient = ReturnType<typeof createClient>;
+export type RedisClient = ReturnType<typeof createClient>;
 
 export const RedisClientService = Symbol.for('RedisClient');
-
-export default RedisClient;
