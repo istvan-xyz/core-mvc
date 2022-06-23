7aea927ac0fd0da4ad7f5ce9e6a409b26459bbdb
István Antal <istvan@antal.xyz>
2022-06-23 08:29:27 +0100

    Remove checked in outputs

--- jwt/decodeToken.d.ts
+++
@@ -1,3 +0,0 @@
-declare const decodeToken: <T>(token: string) => T;
-export default decodeToken;
-//# sourceMappingURL=decodeToken.d.ts.map
--- jwt/userDataFromToken.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const userFromTokenData: <T>(userClass: new () => T, data: Partial<T>) => T;
-//# sourceMappingURL=userDataFromToken.d.ts.map
