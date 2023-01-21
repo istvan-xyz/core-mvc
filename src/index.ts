ce09b7eb08e3ece24e8bf2f40bbefa7286f0e950
István Antal <istvan@antal.xyz>
2023-01-21 09:48:58 +0000

    Expose instrumented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './redis';
 export * from './util/assert';
 export * from './util/log';
+export * from './instrumented';
 export * from './provideSingleton';
 export * from './application';
