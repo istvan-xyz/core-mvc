605276090f73ad465ef7e9c3ec0a98c76279efb9
István Antal <istvan@antal.xyz>
2023-02-01 16:25:39 +0000

    Add RedisSubscriberClientService

--- src/modules/redis.ts
+++ src/modules/redis.ts
@@ -1,33 +1,39 @@
 import { AsyncContainerModule } from 'inversify';
 import { createClient } from '@node-redis/client';
-import { RedisClientService } from '../redis';
+import { RedisClientService, RedisSubscriberClientService } from '../redis';
 import assert from '../util/assert';
 
 const redis = new AsyncContainerModule(async bind => {
-    assert(process.env.REDIS_HOST, 'REDIS_HOST must be defined');
+    const createRedisClient = async () => {
+        assert(process.env.REDIS_HOST, 'REDIS_HOST must be defined');
 
-    const client = createClient({
-        url: `redis://${process.env.REDIS_HOST}:${
-            process.env.REDIS_PORT || '6379'
-        }`,
-        socket: {
-            reconnectStrategy(retries: number) {
-                if (retries < 60) {
-                    return 1_000;
-                }
+        const client = createClient({
+            url: `redis://${process.env.REDIS_HOST}:${
+                process.env.REDIS_PORT || '6379'
+            }`,
+            socket: {
+                reconnectStrategy(retries: number) {
+                    if (retries < 60) {
+                        return 1_000;
+                    }
 
-                return new Error('No retries left');
+                    return new Error('No retries left');
+                },
             },
-        },
-    });
+        });
 
-    client.on('error', (error: unknown) => {
-        throw error;
-    });
+        client.on('error', (error: unknown) => {
+            throw error;
+        });
 
-    await client.connect();
+        await client.connect();
+        return client;
+    };
 
-    bind(RedisClientService).toConstantValue(client);
+    bind(RedisClientService).toConstantValue(await createRedisClient());
+    bind(RedisSubscriberClientService).toConstantValue(
+        await createRedisClient()
+    );
 });
 
 export default redis;
--- src/redis.ts
+++ src/redis.ts
@@ -3,3 +3,5 @@ import type { createClient } from '@node-redis/client';
 export type RedisClient = ReturnType<typeof createClient>;
 
 export const RedisClientService = Symbol.for('RedisClient');
+
+export const RedisSubscriberClientService = Symbol.for('RedisSubscriberClient');
